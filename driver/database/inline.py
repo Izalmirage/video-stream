abbbac90bb9377e880bed87c9957afff13c56d43
izal <94070031+Izalmirage@users.noreply.github.com>
2022-03-12 14:26:40 +0700

    Create inline.py

---
+++ driver/database/inline.py
@@ -0,0 +1,33 @@
+from pyrogram.types import (InlineQueryResultArticle,
+                            InputTextMessageContent)
+
+answer = []
+
+answer.extend(
+    [
+        InlineQueryResultArticle(
+            title="Pause Stream",
+            description=f"Pause the current playout on group call.",
+            thumb_url="https://telegra.ph/file/c0a1c789def7b93f13745.png",
+            input_message_content=InputTextMessageContent("/pause"),
+        ),
+        InlineQueryResultArticle(
+            title="Resume Stream",
+            description=f"Resume the ongoing playout on group call.",
+            thumb_url="https://telegra.ph/file/02d1b7f967ca11404455a.png",
+            input_message_content=InputTextMessageContent("/resume"),
+        ),
+        InlineQueryResultArticle(
+            title="Skip Stream",
+            description=f"Skip to next track. | For Specific track number: /skip [number] ",
+            thumb_url="https://telegra.ph/file/98b88e52bc625903c7a2f.png",
+            input_message_content=InputTextMessageContent("/skip"),
+        ),
+        InlineQueryResultArticle(
+            title="End Stream",
+            description="Stop the ongoing playout on group call.",
+            thumb_url="https://telegra.ph/file/d2eb03211baaba8838cc4.png",
+            input_message_content=InputTextMessageContent("/stop"),
+        ),
+    ]
+)
